acaf0ee02560e60f90ab34b40032df97b4867aad
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2024-08-05 00:50:07 +0530

    Find whether tree is balanced or not

--- src/BinaryTree_2/BinaryTree_2_Use.java
+++ src/BinaryTree_2/BinaryTree_2_Use.java
@@ -15,21 +15,106 @@ class TreeNode {
 }
 
 public class BinaryTree_2_Use {
-	
-	
+
+
 	// TC = O(n)
 	// SC = O(n) stack memory due to recursion
-	
+
 	// NOTE = We can also use level order traversal to find the depth i.e height of binary tree
 	public static int getHeight(TreeNode root) {
-        if(root == null) return 0;
-        if(root.left == null && root.right == null) return 1;
+		if(root == null) return 0;
+		if(root.left == null && root.right == null) return 1;
 
-        int left = 1 + getHeight(root.left);
-        int right = 1 + getHeight(root.right);
+		int left = 1 + getHeight(root.left);
+		int right = 1 + getHeight(root.right);
 
-        return Math.max(left, right);
-    }
+		return Math.max(left, right);
+	}
+
+
+	// TC = O(n^2)
+	// SC = O(1)
+
+	public boolean isBalanced(TreeNode root) 
+	{
+		if(root == null)
+		{
+			return true;
+		}
+		if(root.left == null && root.right == null)
+		{
+			return true;
+		}
+
+		if(Math.abs(height(root.right)-height(root.left)) > 1)
+		{
+			return false;
+		}
+		boolean left = isBalanced(root.left);
+		boolean right = isBalanced(root.right);
+		if(left && right)
+		{
+			return true;
+		}
+		return false;
+
+
+	}
+
+	public int height(TreeNode root)
+	{
+		if(root == null)
+		{
+			return 0;
+		}
+		if(root.left == null && root.right == null)
+		{
+			return 1;
+		}
+		int leftH = height(root.left);
+		int rightH = height(root.right);
+		int h = Math.max(leftH, rightH);
+		return 1+h;
+	}
+
+	// TC = O(n)
+	// SC = O(1)
+	// Using height function to find the balance starting from bottom
+	public boolean isBalancedBetter(TreeNode root) 
+	{
+		return heightBetter(root) != -1;
+	}
+
+	public int heightBetter(TreeNode root)
+	{
+		if(root == null)
+		{
+			return 0;
+		}
+		if(root.left == null && root.right == null)
+		{
+			return 1;
+		}
+		int leftH = heightBetter(root.left);
+
+		if(leftH == -1){
+			return -1;
+		}
+		int rightH = heightBetter(root.right);
+
+		if(rightH == -1){
+			return -1;
+		}
+
+		if(Math.abs(leftH - rightH) > 1){
+			return -1;
+		}
+		int h = Math.max(leftH, rightH);
+		return 1+h;
+	}
+
+
+
 
 	public static void main(String[] args) {
 
