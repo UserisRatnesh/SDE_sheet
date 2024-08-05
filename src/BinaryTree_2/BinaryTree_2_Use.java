22266c82d9e38d9ff67fd12e1cf7a632ee53963c
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2024-08-06 02:14:27 +0530

    Check if two trees are completely indentical or not

--- src/BinaryTree_2/BinaryTree_2_Use.java
+++ src/BinaryTree_2/BinaryTree_2_Use.java
@@ -141,6 +141,21 @@ public class BinaryTree_2_Use {
 
     }
 
+	// TC = O(n)
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null)
+        {
+            return true;
+        }
+        if(p == null || q == null)
+        {
+            return false;
+        }
+
+        // if both are not null
+        return ((p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+        
+    }
 
 
 	public static void main(String[] args) {
