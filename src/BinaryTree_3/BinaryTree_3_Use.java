da7132fb2c4072b4d9459dbc8a887e90857601c3
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2024-08-09 21:06:31 +0530

    Make the binary tree mirrot of itself

--- src/BinaryTree_3/BinaryTree_3_Use.java
+++ src/BinaryTree_3/BinaryTree_3_Use.java
@@ -15,30 +15,60 @@ class TreeNode {
 }
 
 public class BinaryTree_3_Use {
-	
-	
+
+
 	// TC = O(n)
 	// SC = O(n) recursion stack
 	public int maxPathSum(TreeNode root) {
 
-        int[] max = new int[1];
-        max[0] = Integer.MIN_VALUE;
-        helper(root, max); // Bottom up approach
-        return max[0];
+		int[] max = new int[1];
+		max[0] = Integer.MIN_VALUE;
+		helper(root, max); // Bottom up approach
+		return max[0];
 
-    }
+	}
 
-    public int helper(TreeNode root, int[] max){
-        if(root == null) return 0;
+	public int helper(TreeNode root, int[] max){
+		if(root == null) return 0;
 
-        int maxL = Math.max(0, helper(root.left, max));
-        int maxR = Math.max(0, helper(root.right, max));
+		int maxL = Math.max(0, helper(root.left, max));
+		int maxR = Math.max(0, helper(root.right, max));
 
-        max[0] = Math.max(max[0], root.val + maxL + maxR);
+		max[0] = Math.max(max[0], root.val + maxL + maxR);
 
-        // return the path having maxSum
-        return root.val + Math.max(maxL, maxR);
-    }
+		// return the path having maxSum
+		return root.val + Math.max(maxL, maxR);
+	}
+
+
+	// TC = O(n) => reach every child only once
+	public static void mirror(TreeNode node) {
+
+		if(node == null){
+			return;
+		}
+		TreeNode newRoot = new TreeNode(node.val);
+		newRoot.right = getChild(node.left);
+		newRoot.left = getChild(node.right);
+
+		node.left = newRoot.left;
+		node.right = newRoot.right;
+
+		return;
+
+	}
+
+	public static TreeNode getChild(TreeNode root){
+		if(root == null){
+			return null;
+		}
+
+		TreeNode newRoot = new TreeNode(root.val);
+		newRoot.left = getChild(root.right);
+		newRoot.right = getChild(root.left);
+
+		return newRoot;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
