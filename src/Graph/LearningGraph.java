887d447cecee2c01106dd2d4cf80933f70c24c3f
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2024-08-31 00:45:44 +0530

    Topological sort using dfs and Kahn's algorithm(bfs)

---
+++ src/Graph/LearningGraph.java
@@ -0,0 +1,91 @@
+package Graph;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+public class LearningGraph {
+	
+	
+	// TC = O(v*E), since it is a dfs 
+	public int[] topoSort(int V, List<Integer>[] adj) {
+
+        Stack<Integer> stk = new Stack<>();
+
+        boolean[] visited = new boolean[V];
+
+        for(int i=0; i<V; i++){
+            if(!visited[i]){
+                dfs(i, adj, stk, visited);
+            }
+        }
+
+        int[] ans = new int[V];
+        int index = 0;
+
+        while(!stk.isEmpty()){
+            ans[index++] = stk.pop();
+        }
+
+        return ans;
+
+        
+    }
+
+    public void dfs(int v, List<Integer>[] adj, Stack<Integer> stk, boolean[] visited){
+
+        visited[v] = true;
+        
+        for(Integer node : adj[v]){
+            if(!visited[node]){
+                dfs(node, adj, stk, visited);
+            }
+        }
+
+        stk.add(v);
+    }
+    
+    // Topo sort using bfs
+    // TC = O(v+E)
+    public int[] topoSortBFS(int V, List<Integer>[] adj) {
+
+        int[] indegree = new int[V];
+
+        for(int i=0; i<V; i++){
+            for(Integer child : adj[i]){
+                indegree[child]++;
+            }
+
+        }
+
+        Queue<Integer> que = new LinkedList<>();
+        for(int i=0; i<V; i++){
+            if(indegree[i] == 0){
+                que.add(i);
+            }
+        }
+
+        int[] ans = new int[V];
+        int index = 0;
+        while(!que.isEmpty()){
+            int node = que.poll();
+            ans[index++] = node;
+            for(Integer child : adj[node]){
+                indegree[child]--;
+                if(indegree[child] == 0){
+                    que.add(child);
+                }
+            }
+        }
+
+        return ans;
+        
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
